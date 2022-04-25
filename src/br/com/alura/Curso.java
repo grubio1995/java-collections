73d3ea9e0da280387e52bcd5fef07fb2ba75fd4f
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2022-04-24 22:13:23 -0300

    Aplicando o set ao modelo

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,14 +1,17 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<>();
+	private Set<Aluno> alunos = new HashSet<>();
 
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
@@ -33,6 +36,14 @@ public class Curso {
 
 	public Integer getTempoTotal() {
 		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+	
+	public Set<Aluno> getAlunos() {
+		return Collections.unmodifiableSet(alunos);
+	}
+	
+	public void matricula(Aluno aluno) {
+		this.alunos.add(aluno);
 	}
 
 	@Override
